bf0bebb23d7308032f4b41ebe4de02e5ea64ecd4
Julio Banda <julitop_55@hotmail.com>
2019-03-15 16:26:51 -0600

    Tabla de multiplicar

--- Pruebas.py
+++ Pruebas.py
@@ -1,6 +1,30 @@
+#Fibonacci serie
 a, b = 0, 1
 
 while a < 100:
     print(a, end=", ")
     a, b = b, a + b
 
+#if statement
+print()
+number = 3 #int(input("\n\n\nInsert a number: "))
+if number < 0:
+    print("That's a negative number")
+elif number == 0:
+    print("Zero")
+elif number == 1:
+    print("Single")
+elif number > 1:
+    print("Other")
+
+#for statement
+print()
+words = ['statement', 'cat', 'dog', 'defenestrate']
+for word in words[:]:
+    print(word, 'has', len(word), 'letters')
+
+#Tabla de multiplicar
+print("\n\n")
+tableOf = int(input("Tabla de multiplicar del: "))
+for i in range(1, 11):
+    print(i*tableOf)
