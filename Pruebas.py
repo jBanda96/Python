518b6431c053b03fb9ac8f9084c7d21a51f86d33
Julio Banda <julitop_55@hotmail.com>
2019-03-19 14:54:47 -0600

    Function declaration

--- Pruebas.py
+++ Pruebas.py
@@ -37,3 +37,23 @@ for number in range(2, 10):
         print("Found an even number", number)
         continue
     print("Found a number", number)
+
+#Print statement
+print("Hola", end=",\n")
+print("-" * 40)
+
+#Función
+print("\n\n")
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+ask_ok('Do you really want to quit?')
