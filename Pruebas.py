91667e9ba6b4fc8a7a5242914cfeec730b9d5236
Julio Banda <julitop_55@hotmail.com>
2019-03-20 15:54:12 -0600

    Input types

--- Pruebas.py
+++ Pruebas.py
@@ -1,6 +1,10 @@
+import random
+#import antigravity
+
 #Fibonacci serie
 a, b = 0, 1
 
+print("\n" * 2)
 while a < 100:
     print(a, end=", ")
     a, b = b, a + b
@@ -25,10 +29,15 @@ for word in words[:]:
 
 #Tabla de multiplicar
 print("\n\n")
-tableOf = int(input("Tabla de multiplicar del: "))
-for i in range(1, 11):
-    print(i*tableOf)
-    if i == 4: break
+try:
+    tableOf =  3 #int(input("Tabla de multiplicar del: "))
+
+    print()
+    for i in range(1, 11):
+        print(tableOf, '*', i, '=' ,i*tableOf)
+
+except ValueError as error:
+    print(error, 'NaN')
 
 #Números primos
 print("\n\n")
@@ -39,8 +48,40 @@ for number in range(2, 10):
     print("Found a number", number)
 
 #Print statement
+print("\n" * 2)
+print("-" * 40)
 print("Hola", end=",\n")
 print("-" * 40)
+
+#For statement asking for input integers
+print("\n" * 2)
+
+lista = [int(x) for x in input('Ingresa números separados por espacio: ').split()]
+print('Tipo:',
+      type(lista),
+      lista
+      )
+
+num = int(input("Ingresa un número: "))
+str = str(input("Ingresa un string: "))
+lst = list(input("Ingresa una lista: "))
+
+print(type(num), type(str), type(lst))
+
+#Vulnerabilities in input()
+print("\n" * 2)
+secret_number = random.randint(1,2)
+print ("Pick a number between 1 to 500")
+#print(f"Number to guess: {secret_number}")
+print("Number to guess: {}".format(secret_number))
+while True:
+    res = int(input("Guess the number: "))
+    if res==secret_number:
+        print ("You win")
+        break
+    else:
+        print ("You lose")
+        continue
 
 #Función
 print("\n\n")
@@ -56,4 +97,4 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
             raise ValueError('invalid user response')
         print(reminder)
 
-ask_ok('Do you really want to quit?')
+ask_ok('Do you really want to quit? ')
